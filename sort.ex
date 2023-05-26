eb73972704701f3514a2b390f62a9a686965976f
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-26 11:12:12 -0400

    chapter 4 code additions

--- sort.ex
+++ sort.ex
@@ -6,22 +6,44 @@ defmodule Sort do
 
     {list_a, list_b} = Enum.split(list, half_size)
 
-    merge(
+    ascending_merge(
       ascending(list_a),
       ascending(list_b)
     )
   end
 
-  defp merge([], list_b), do: list_b
-  defp merge(list_a, []), do: list_a
-  defp merge([head_a | tail_a], list_b = [head_b | _]) when head_a <= head_b do
-    IO.inspect(list_a)
-    IO.inspect(list_b)
-    [head_a | merge(tail_a, list_b)]
+  def descending([]), do: []
+  def descending([a]), do: [a]
+  def descending(list) do
+    half_size = div(Enum.count(list), 2)
+
+    {list_a, list_b} = Enum.split(list, half_size)
+
+    descending_merge(
+      descending(list_a),
+      descending(list_b)
+    )
   end
-  defp merge(list_a = [head_a | _], [head_b | tail_b]) when head_a > head_b do
-    IO.inspect(list_a)
-    IO.inspect(list_b)
-    [head_b | merge(list_a, tail_b)]
+
+  defp ascending_merge([], list_b), do: list_b
+  defp ascending_merge(list_a, []), do: list_a
+  defp ascending_merge([head_a | tail_a], list_b = [head_b | _]) when head_a <= head_b do
+
+    [head_a | ascending_merge(tail_a, list_b)]
+  end
+  defp ascending_merge(list_a = [head_a | _], [head_b | tail_b]) when head_a > head_b do
+
+    [head_b | ascending_merge(list_a, tail_b)]
+  end
+
+  defp descending_merge([], list_b), do: list_b
+  defp descending_merge(list_a, []), do: list_a
+  defp descending_merge([head_a | tail_a], list_b = [head_b | _]) when head_a > head_b do
+
+    [head_a | descending_merge(tail_a, list_b)]
+  end
+  defp descending_merge(list_a = [head_a | _], [head_b | tail_b]) when head_a <= head_b do
+
+    [head_b | descending_merge(list_a, tail_b)]
   end
 end
