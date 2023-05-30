207c40be7e7297d46c2e8cc09e8951f1aa9161c3
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-30 08:59:47 -0400

    chapter 5 code additions

--- screws_factory.ex
+++ screws_factory.ex
@@ -28,6 +28,8 @@ defmodule ScrewsFactory do
     |> Stream.flat_map(&add_thread/1)
     |> Stream.chunk(100)
     |> Stream.flat_map(&add_head/1)
+    |> Stream.chunk(50)
+    |> Stream.flat_map(&add_packaging/1)
     |> Enum.each(&output/1)
   end
 
@@ -39,6 +41,11 @@ defmodule ScrewsFactory do
   defp add_head(pieces) do
     Process.sleep(100)
     Enum.map(pieces, &("o" <> &1))
+  end
+
+  defp add_packaging(pieces) do
+    Process.sleep(70)
+    Enum.map(pieces, &("|" <> &1 <> "|"))
   end
 
   defp output(screw) do
