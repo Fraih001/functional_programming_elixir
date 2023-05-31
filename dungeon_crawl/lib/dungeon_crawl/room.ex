194ea00f08702b28ad0beaa3c5db9cc6ffc90be4
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-31 09:53:48 -0400

    chapter 6 code additions

--- dungeon_crawl/lib/dungeon_crawl/room.ex
+++ dungeon_crawl/lib/dungeon_crawl/room.ex
@@ -1,14 +1,21 @@
 defmodule DungeonCrawl.Room do
   alias DungeonCrawl.Room
+  alias DungeonCrawl.Room.Triggers
 
   import DungeonCrawl.Room.Action
 
-  defstruct description: nil, actions: []
+  defstruct description: nil, actions: [], trigger: nil
 
   def all, do: [
     %Room{
       description: "You found a quiet place. Looks safe for a little nap.",
-      actions: [forward(), rest()]
+      actions: [forward(), rest()],
+      trigger: Triggers.Exit
+    },
+    %Room{
+      description: "You can see an enemy blocking your path.",
+      actions: [forward()],
+      trigger: Triggers.Enemy
     },
   ]
 end
