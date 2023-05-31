3d45a617aa01af3394f05447eee7d4689facb87c
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-31 10:54:30 -0400

    chapter 6 - add additional rooms to dungeon crawl

--- dungeon_crawl/lib/dungeon_crawl/room.ex
+++ dungeon_crawl/lib/dungeon_crawl/room.ex
@@ -10,12 +10,32 @@ defmodule DungeonCrawl.Room do
     %Room{
       description: "You found a quiet place. Looks safe for a little nap.",
       actions: [forward(), rest()],
-      trigger: Triggers.Exit
+      trigger: Triggers.Exit,
     },
     %Room{
       description: "You can see an enemy blocking your path.",
       actions: [forward()],
-      trigger: Triggers.Enemy
+      trigger: Triggers.Enemy,
+    },
+    %Room{
+      description: "You found a quiet place. Looks safe for a little nap.",
+      actions: [forward(), rest()],
+      trigger: Triggers.Rest,
+    },
+    %Room{
+      description: "You found a quiet place, but there may still be enemies near.",
+      actions: [forward(), rest()],
+      trigger: Triggers.EnemyHidden,
+    },
+    %Room{
+      description: "You found a quiet place, but it could be a trap!",
+      actions: [forward(), search()],
+      trigger: Triggers.Trap,
+    },
+    %Room{
+      description: "You found a quiet place, let's search and see if we can find some treasure!",
+      actions: [forward(), search()],
+      trigger: Triggers.Treasure,
     },
   ]
 end
