102e462cfd5addc4fd94cb9deaf030f5ceb01c66
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-31 12:07:29 -0400

    chapter 7 - use try, raise, rescue for error handling

--- dungeon_crawl/lib/dungeon_crawl/cli/hero_choice.ex
+++ dungeon_crawl/lib/dungeon_crawl/cli/hero_choice.ex
@@ -1,18 +1,12 @@
 defmodule DungeonCrawl.CLI.HeroChoice do
   alias Mix.Shell.IO, as: Shell
   import DungeonCrawl.CLI.BaseCommands
+
     def start do
       Shell.cmd("clear")
       Shell.info("Start by choosing your hero:")
-      heroes = DungeonCrawl.Heroes.all()
-      find_hero_by_index = &Enum.at(heroes, &1)
-
-      heroes
-        |> display_options
-        |> generate_question
-        |> Shell.prompt
-        |> parse_answer
-        |> find_hero_by_index.()
+      DungeonCrawl.Heroes.all()
+        |> ask_for_option
         |> confirm_hero
     end
 
